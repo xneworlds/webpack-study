f3ad7e252b3f200f892e843fb7d09844605941e1
xneworlds <193116411@qq.com>
2021-04-13 21:40:44 +0800

    feat: 压缩css

---
+++ 11.压缩css/webpack.config.js
@@ -0,0 +1,42 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "js/build.js",
+    path: resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              postcssOptions: {
+                plugins: [[require("postcss-preset-env"), {}]],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/build.css",
+    }),
+    /** 压缩css */
+    new OptimizeCssAssetsWebpackPlugin(),
+  ],
+  mode: "development",
+};
