0ae266cbd4746659fc7d0b2a3f1164ff9664a2c3
xneworlds <193116411@qq.com>
2021-04-13 23:17:36 +0800

    feat: 生产环境配置

---
+++ 16.生产环境配置/webpack.config.js
@@ -0,0 +1,113 @@
+const { resolve } = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+
+/** 复用loader */
+const commonCssLoader = [
+  MiniCssExtractPlugin.loader,
+  "css-loader",
+  {
+    loader: "post-loader",
+    options: {
+      postcssOptions: [[require("postcss-preset-env"), {}]],
+    },
+  },
+];
+
+module.exports = {
+  entry: "./src/js/index.js",
+  output: {
+    filename: "js/build.js",
+    path: resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [...commonCssLoader],
+      },
+      {
+        test: /\.less$/,
+        use: [...commonCssLoader, "less-loader"],
+      },
+      /*
+        正常来讲，一个文件只能被一个loader处理
+        当一个文件要被多个loader处理，那么一定要指定loader执行的先后顺序
+        先执行eslint再执行babel（用enforce）
+      */
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        /** 优先执行 */
+        enforce: "pre",
+        loader: "eslint-loader",
+        options: {
+          fix: true,
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-env",
+              {
+                useBuiltIns: "usage",
+                corejs: { version: 3 },
+                targets: {
+                  chrome: "60",
+                  firefox: "60",
+                  ie: "9",
+                  safari: "10",
+                  edge: "17",
+                },
+              },
+            ],
+          ],
+        },
+      },
+      {
+        test: /\.(jpg|png|gif)$/,
+        loader: "url-loader",
+        options: {
+          limit: 8 * 1024,
+          name: "[hash:10].[ext]",
+          esModule: false,
+          outputPath: "imgs",
+        },
+      },
+      {
+        test: /\.html$/,
+        loader: "html-loader",
+        options: {
+          esModule: false,
+        },
+      },
+      {
+        exclude: /\.(html|js|css|less|jpg|png|gif)$/,
+        loader: "file-loader",
+        options: {
+          outputPath: "media",
+          name: "[hash:10].[ext]",
+        },
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      minify: {
+        collapseWhitespace: true,
+        removeComments: true,
+      },
+    }),
+    new MiniCssExtractPlugin({
+      filename: "css/build.css",
+    }),
+    new OptimizeCssAssetsWebpackPlugin(),
+  ],
+  mode: "production",
+};
